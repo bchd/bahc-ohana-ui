6b7d81c54a1089f9d9994140a3fa94fa85705903
Jenny Wagner <jennywagg@gmail.com>
2019-03-28 09:42:54 -0400

    AHC-1150 test the other wait time options

--- spec/views/component/detail/_service_wait_spec.rb
+++ spec/views/component/detail/_service_wait_spec.rb
@@ -17,7 +17,51 @@ RSpec.describe 'component/detail/service_wait' do
     expect(rendered).to have_selector('.fa.fa-check-circle-o.available')
   end
 
-  it 'will show the correct wait time' do
-    expect(rendered).to have_content('Available Today')
+  context "when the wait time is 'Available Today'" do
+    let(:service) { build(:service, wait_time: 'Available Today') }
+
+    before do
+      render 'component/detail/service_wait', service: service
+    end
+
+    it 'will show the correct wait time' do
+      expect(rendered).to have_content('Available Today')
+    end
+  end
+
+  context "when the wait time is '2-3 Day Wait'" do
+    let(:service) { build(:service, wait_time: '2-3 Day Wait') }
+
+    before do
+      render 'component/detail/service_wait', service: service
+    end
+
+    it 'will show the correct wait time' do
+      expect(rendered).to have_content('2-3 Day Wait')
+    end
+  end
+
+  context "when the wait time is '1 Week Wait'" do
+    let(:service) { build(:service, wait_time: '1 Week Wait') }
+
+    before do
+      render 'component/detail/service_wait', service: service
+    end
+
+    it 'will show the correct wait time' do
+      expect(rendered).to have_content('1 Week Wait')
+    end
+  end
+
+  context "when the wait time is 'Unknown'" do
+    let(:service) { build(:service, wait_time: 'Unknown') }
+
+    before do
+      render 'component/detail/service_wait', service: service
+    end
+
+    it 'will show the correct wait time' do
+      expect(rendered).to have_content('Unknown')
+    end
   end
 end
