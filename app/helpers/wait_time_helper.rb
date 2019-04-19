a5c2d0d015cb203b47403e88acb598873370738f
Doug Price <dprice@fearless.tech>
2019-04-19 10:43:37 -0400

    AHC-1301: changed the wait time to a translation key

--- app/helpers/wait_time_helper.rb
+++ app/helpers/wait_time_helper.rb
@@ -1,14 +1,20 @@
 module WaitTimeHelper
+  def wait_time_text(wait_time)
+    return translate('service_fields.availability.unknown') unless wait_time
+
+    translate("service_fields.availability.#{wait_time}")
+  end
+
   # rubocop:disable Metrics/MethodLength
   def wait_time_icon_classes(service)
     case service&.wait_time
-    when 'Available Today'
+    when 'available_today'
       'fa fa-check-circle-o available'
-    when 'Next Day Service'
+    when 'next_day_service'
       'fa fa-chevron-circle-right next-day'
-    when '2-3 Day Wait'
+    when 'two_three_day_wait'
       'fa fa-clock-o short-wait'
-    when '1 Week Wait'
+    when 'one_week_wait'
       'fa fa-times-circle-o long-wait'
     else
       'fa fa-question-circle-o unknown'
