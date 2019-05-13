4c76073292801c6bf70f2fc45cf3cff492b17c03
Jenny Wagner <jennywagg@gmail.com>
2019-05-13 16:55:34 -0400

    AHC-268 Update map styling

--- app/javascript/app/util/map/MapSizeControl.js
+++ app/javascript/app/util/map/MapSizeControl.js
@@ -17,7 +17,7 @@ function MapSizeControl(selector) {
   eventObserver.attach(this);
 
   // The DOM element that controls the expanding/contracting of the map.
-  var _buttonElm = document.querySelector(selector);
+  // var _buttonElm = document.querySelector(selector);
 
   // Whether the map is at its max size or not.
   var _atMaxSize = false;
@@ -27,9 +27,9 @@ function MapSizeControl(selector) {
   var SMALLER_MAP_TEXT = "<i class='fa fa-plus-square'></i> Larger map";
 
   function _init() {
-    _buttonElm.innerHTML = SMALLER_MAP_TEXT;
-    _buttonElm.removeAttribute('disabled');
-    _buttonElm.addEventListener('click', _buttonElmClicked, false);
+    // _buttonElm.innerHTML = SMALLER_MAP_TEXT;
+    // _buttonElm.removeAttribute('disabled');
+    // _buttonElm.addEventListener('click', _buttonElmClicked, false);
   }
 
   // Map size control was clicked. This control toggles the large & small map.
