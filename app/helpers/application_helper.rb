2aed515a390bef47b8c4445b2081087d3c761eb8
Tina G <tina@smartlogic.io>
2020-04-13 09:12:51 -0400

    Redcarpet +Added redcarpet to render markdown

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -36,4 +36,18 @@ module ApplicationHelper
   def cache_key_for(hash)
     Digest::MD5.hexdigest(hash.to_s)
   end
+
+  # Render Markdown
+  def render_markdown(content)
+    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML,
+                                       no_intra_emphasis: true,
+                                       fenced_code_blocks: true,
+                                       disable_indented_code_blocks: true,
+                                       autolink: true,
+                                       tables: true,
+                                       underline: true,
+                                       highlight: true
+                                      )
+    return markdown.render("This is *bongos*, indeed.").html_safe
+  end
 end
