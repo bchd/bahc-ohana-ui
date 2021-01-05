bb4fa7e7a59b7f887f666ef6379222aea5e5b767
Alejandra <alejandr@gmail.com>
2021-01-05 15:25:29 -0500

    Update locations_controller.rb
    remove extra space

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -13,7 +13,6 @@ class LocationsController < ApplicationController
     # Populate the keyword search field with the original term
     # as typed by the user, not the translated word.
     # params[:keyword] = translator.original_keyword
-
     cache_page(locations) if locations.present?
   end
 
