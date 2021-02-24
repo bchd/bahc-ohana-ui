07c2955c5232e4c5e5063a02a4b391ba0b68c506
Justus Eapen <justuseapen@gmail.com>
2021-02-23 23:09:32 -0500

    Removes type conditions from main categories

--- app/helpers/filter_categories_helper.rb
+++ app/helpers/filter_categories_helper.rb
@@ -6,12 +6,12 @@ module FilterCategoriesHelper
 
   def main_categories_array
     fetch_categories if @categories.nil?
-    @categories.select { |cat| cat[:depth] == 0  and cat[:type] == "service" }.flatten.uniq.map{ |cat| [cat.name, cat.id]}
+    @categories.select { |cat| cat[:depth] == 0 }.flatten.uniq.map{ |cat| [cat.name, cat.id]}
   end
 
   def categories_for_select
     fetch_categories if @categories.nil?
-    @categories.select { |cat| cat[:depth] == 0  and cat[:type] == "service" }.flatten.uniq.map{ |cat| [cat.name, cat.name]}
+    @categories.select { |cat| cat[:depth] == 0}.flatten.uniq.map{ |cat| [cat.name, cat.name]}
   end
 
   def category_name_by_id(category_id)
@@ -28,7 +28,7 @@ module FilterCategoriesHelper
 
   def subcategories_by_category(category_id)
     fetch_categories if @categories.nil?
-    @categories.select { |cat| cat[:depth] == 1  and cat[:type] == "service" and cat[:parent_id] == category_id.to_i }.flatten.uniq.map{ |cat| cat.name }
+    @categories.select { |cat| cat[:depth] == 1 and cat[:parent_id] == category_id.to_i }.flatten.uniq.map{ |cat| cat.name }
   end 
 
   def subcategrory_id_by_name(subcategory_name, main_category_id)
