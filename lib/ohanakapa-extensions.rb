61af8c905a43b9ea28a5323a2a4c527f1db0542d
Stone Filipczak <stone@smartlogic.io>
2021-12-08 17:04:03 -0500

    get all languages from api

--- lib/ohanakapa-extensions.rb
+++ lib/ohanakapa-extensions.rb
@@ -5,11 +5,17 @@ module Ohanakapa
         get("recommended_tags")
       end
     end
+    module Languages
+      def languages
+        get("languages")
+      end
+    end
   end
 end
 
 module Ohanakapa
   class Client
     include Ohanakapa::Client::RecommendedTags
+    include Ohanakapa::Client::Languages
   end
 end
